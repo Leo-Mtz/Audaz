815108d769ecc745c82f6e81c9709593d4bc0aca
Leo <leo.auxdei.jr@gmail.com>
2024-05-01 15:12:29 -0600

    Concatenacion de nombre y apellidos en formulario de entrada y salidas

--- ventas/views/entradas/index.php
+++ ventas/views/entradas/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\EntradasSearch */
@@ -19,68 +20,58 @@ $this->params['breadcrumbs'][] = $this->title;
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
     <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            //'id_entradas',
-            'fecha',
-
-            [
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
+        //'id_entradas',
+        'fecha',
+        [
             'attribute'=>'id_empleado',
-            'value'=>function($model,$index,$dataColumn)
-            {
+            'value'=>function($model) {
                 return $model->empleados->id_empleado;
             },
         ],
         [
             'attribute'=>'id_evento',
-            'value'=>function($model,$index,$dataColumn)
-            {
+            'value'=>function($model) {
                 return $model->eventos->id_evento;
             }
-
         ],
         [
             'attribute' => 'id_producto',
-            'value' =>function($model,$index,$dataColumn)
-
-            {
+            'value' => function($model) {
                 return $model->productos->id_producto;
             },
-
         ],
-
         'cantidad_entradas',
-            
 
-        'class' => 'yii\grid\ActionColumn',
-        'header' => 'Acciones',
-        'headerOptions' => ['style' => 'color:#007bff'],
-        'contentOptions' => ['style' => 'width:12%;'],
-        'template' => '{view}{update}{delete}',
-        'buttons' => [
-            'view' => function ($url, $model) {
-                $url = Url::to(['entradas/view','id'=>$model->id_entradas]);
-                return Html::a('<span class="fa fa-search"></span>', $url, ['title' => 'Ver','style' => 'margin-right:10px']);
-            },
-
-            'update' => function ($url, $model) {
-                $url = Url::to(['entradas/update','id'=>$model->id_entradas]);
-                return Html::a('<span class="fa fa-edit"></span>', $url, ['title' => 'Actualizar','style' => 'margin-right:10px']);
-            },
-                                
-            'delete' => function ($url, $model) {
-                $url = Url::to(['entradas/delete','id'=>$model->id_entradas]);
-                return Html::a('<span class="fa fa-times"></span>', $url, ['title' => 'Borrar','data-confirm' => Yii::t('yii', '¿Seguro que desea eliminar esta entrada?'), 'data-method'  => 'post']);
-            }
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'header' => 'Acciones',
+            'headerOptions' => ['style' => 'color:#007bff'],
+            'contentOptions' => ['style' => 'width:12%;'],
+            'template' => '{view} {update} {delete}',
+            'buttons' => [
+                'view' => function ($url, $model) {
+                    $url = Url::to(['entradas/view','id'=>$model->id_entradas]);
+                    return Html::a('<span class="fa fa-search"></span>', $url, ['title' => 'Ver','style' => 'margin-right:10px']);
+                },
+                'update' => function ($url, $model) {
+                    $url = Url::to(['entradas/update','id'=>$model->id_entradas]);
+                    return Html::a('<span class="fa fa-edit"></span>', $url, ['title' => 'Actualizar','style' => 'margin-right:10px']);
+                },
+                'delete' => function ($url, $model) {
+                    $url = Url::to(['entradas/delete','id'=>$model->id_entradas]);
+                    return Html::a('<span class="fa fa-times"></span>', $url, [
+                        'title' => 'Borrar',
+                        'data-confirm' => Yii::t('yii', '¿Seguro que desea eliminar esta entrada?'),
+                        'data-method' => 'post'
+                    ]);
+                }
+            ],
         ],
     ],
-
-    ]); ?>
-
-
-</div>
+]); ?>
+ 
