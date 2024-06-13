46e2ca48b29b5896104c3c2d16c918e21f15a0c9
Leo <leo.auxdei.jr@gmail.com>
2024-06-13 13:30:37 -0600

    adicion de boton de crear nueva salida en view

--- ventas/views/salidas/view.php
+++ ventas/views/salidas/view.php
@@ -15,8 +15,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id_salidas], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id_salidas], [
+
+        <?= Html::a('Editar', ['update', 'id' => $model->id_salidas], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Nueva Salida', ['create', 'id'=>$model->id_salidas], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Borrar', ['delete', 'id' => $model->id_salidas], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
