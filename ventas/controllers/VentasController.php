b69fe81802d91ab74a8f59235c52929b60c8faa4
Leo <leo.auxdei.jr@gmail.com>
2024-06-17 12:10:25 -0600

    creacion de form para registro de ventas

--- ventas/controllers/VentasController.php
+++ ventas/controllers/VentasController.php
@@ -1,9 +1,15 @@
 <?php
 
+
 namespace app\controllers;
-use yii\base\Model;
-use yii\data\ActiveDataProvider;
+
+use Yii;
 use app\models\Ventas;
+use app\models\VentasSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
 class VentasController extends \yii\web\Controller
 {
     public function actionIndex()
@@ -15,9 +21,16 @@ class VentasController extends \yii\web\Controller
 
     public function actionCreate(){
 
-        $model= new Ventas();
+        $model = new Ventas();
 
-        return $this->render('create');
+        if ($model->load(Yii::$app->request->post())) {
+			$model->save();
+            return $this->redirect(['view', 'id' => $model->id_venta]);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
         
     }
 
