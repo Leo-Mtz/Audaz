7f26ac7cf3ec5befb6afa7510ccf5f38e6cc2d70
Leo <leo.auxdei.jr@gmail.com>
2024-06-27 12:34:33 -0600

    Actualizacion de actionUpdate

--- ventas/controllers/VentasController.php
+++ ventas/controllers/VentasController.php
@@ -106,7 +106,6 @@ class VentasController extends \yii\web\Controller
 
     public function actionDelete($id){
 
-        echo 
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
@@ -115,8 +114,58 @@ class VentasController extends \yii\web\Controller
 
     public function actionUpdate($id){
 
-        $model= new Ventas();
-        return $this->render('update');
+        $model= $this->findModel($id);
+
+        // Set the fecha attribute to the current date
+       $model->fecha = Yii::$app->formatter->asDate('now', 'php:Y-m-d');
+       date_default_timezone_set('America/Mexico_City'); // Set the timezone to Mexico City
+
+       
+       if ($model->load(Yii::$app->request->post())) {
+               $model->save();
+       
+               // Get the automatically generated id_venta
+               $idVenta = $model->id_venta;
+       
+               if (isset(Yii::$app->request->post('Ventas')['productos'])) {
+                   $productos = Yii::$app->request->post('Ventas')['productos'];
+                   foreach ($productos as $producto) {
+                       // Directly insert the product into the Ventas table
+                       $ventaProducto = new Ventas();
+                       $ventaProducto->id_venta = $model->id_venta;
+                       $ventaProducto->id_producto = $producto['id_producto'];
+                       $ventaProducto->cantidad_vendida = $producto['cantidad_vendida'];
+                       $ventaProducto->precio_total_producto = $producto['precio_total_producto'];
+                       $ventaProducto->save();
+                   }
+               }
+       
+               return $this->redirect(['view', 'id' => $model->id_venta]);
+           } else {
+               var_dump($model->errors);
+           }
+   
+
+   
+       $productos = CatProductos::find()->all();
+       $productosDropdown = [];
+       foreach ($productos as $producto) {
+           $productosDropdown[$producto->id_producto] = $producto->sabores->sabor . ' - ' . $producto->presentaciones->presentacion;
+       }
+
+
+       return $this->render('update', [
+
+           'model'=>$model,
+           //'vendedor'=>$vendedor,
+           'productosDropdown'=>$productosDropdown,
+          // 'eventos'=>$eventos
+           
+       ]);
+
+
+
+
     }
 
     
