1da2f98fa87cd6088ab6f016495f182afcbb0be5
Leo <leo.auxdei.jr@gmail.com>
2024-06-27 12:32:34 -0600

    creacion de update form

--- ventas/views/ventas/update.php
+++ ventas/views/ventas/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Ventas */
 
-$this->title = 'Update Ventas: ' . $model->id_venta;
+$this->title = 'Actualizar Venta: ' . $model->id_venta;
 $this->params['breadcrumbs'][] = ['label' => 'Ventas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id_venta, 'url' => ['view', 'id' => $model->id_venta]];
 $this->params['breadcrumbs'][] = 'Update';
